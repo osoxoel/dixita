edf057fe0ea07c08cf94bd5bc3eef3635e59813c
osoxoel <rialxoel@gmail.com>
2025-02-17 16:48:21 +0100

    Add files via upload

---
+++ benvida.py
@@ -0,0 +1,14 @@
+print("Boas tardes")
+nome = input("Como te chamas?")
+print("O teu nome é ",nome)
+
+
+
+
+idade = input("Cantos anos tes?")
+print("Que madurito estas", idade, "anos, que mogollón!!" )
+print ("Feliz día")
+print("Agora matematicas")
+numerito = int (input("introduce un numero") )
+print ("O dobre do teu numero é" , numerito *2 )
+print ("O dobre do teu numero é" , numerito %2 )
