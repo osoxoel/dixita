a127f1337a1f2494da523a77da3d701feebd3409
osoxoel <rialxoel@gmail.com>
2025-02-20 13:50:05 +0100

    Add files via upload

--- tarefa3.py
+++ tarefa3.py
@@ -2,3 +2,11 @@ import math
 print (math.sqrt(45))
 lolo = 18
 print (math.sqrt(lolo))
+import math
+print (math.sqrt(25))
+pepe = 5
+print (math.sqrt(pepe))
+import math
+print (**(110))
+marc = 35
+print (math.sqrt(marc))
