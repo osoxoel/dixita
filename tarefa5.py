29e61c7e68db9b74ba34f4c09387ba1502d535f8
osoxoel <rialxoel@gmail.com>
2025-02-24 16:48:21 +0100

    Add files via upload

---
+++ tarefa5.py
@@ -0,0 +1,23 @@
+print("***************** Compra en RettyShop **************")
+resposta = input("Cantas ducias de ovos queres?")
+respostaNum = int(resposta)
+ovos = 12*respostaNum
+print("Tes que comprar ", ovos, "este número de ovos")
+print("***************** Compra en RettyShop **************")
+
+
+respost = input ("Cantos km fas de paseo??")
+respostNum = int(respost)
+gallina = 1000*respostNum
+print("Ohhh eso son" , gallina, "metros")
+
+resposte = input ("Cantos euros tes?")
+resposteNum = int(resposte)
+leuros = 443* resposteNum
+print("Ohhh eso son", leuros, "grivna")
+
+
+pou = input ("Cantos euros tes?")
+pouNum = int(pou)
+dolars = 1.05*pouNum
+print("Ohhh eso son", dolars , "dolares")
