30eee519d58db69bd16f1854227e5d48855f56c4
osoxoel <rialxoel@gmail.com>
2025-02-27 13:57:42 +0100

    Add files via upload

---
+++ caixarapida.py
@@ -0,0 +1,9 @@
+print("***************** Compra en RettyShop **************")
+primeiro = input("Canto costa o primeiro producto?")
+p1 = int(primeiro)
+segundo = input("Canto costa o segundo producto?")
+p2 = int(segundo)
+terceiro  = input("Canto costa o terceiro producto?")
+p3 = int(terceiro)
+total = p1 + p2 +p3
+print("total a pagar", total, "euros")
