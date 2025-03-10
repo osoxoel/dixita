cbd0a495c174ea38baa5eb7aee6ed0a3e3226e7c
osoxoel <rialxoel@gmail.com>
2025-03-10 16:23:37 +0100

    Add files via upload

---
+++ tarefa6.py
@@ -0,0 +1,22 @@
+
+persoa = input("Como te chamas??")
+if(persoa == "xoel"):
+    print("Bos días ", persoa)
+elif (persoa == "pereira"):
+    print("Fuera de este ordenador")
+
+elif (persoa == "marcos"):
+    print("marcos pirate")
+
+elif (persoa == "pedro"):
+    print("Callate pedro")
+else:
+    print("Tu quien eres?")
+
+
+
+numero = input("Cual es tu numero favorito??")
+if(numero == "10"):
+    print("Ese también es mi numero favorito",numero)
+else:
+    print("Ese numero esta bien pero no me gusta tanto como el 10")
