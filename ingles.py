e8bb432d3811df9b2d576ad2b1d891fa44b557e2
osoxoel <rialxoel@gmail.com>
2025-03-11 13:52:58 +0100

    Add files via upload

---
+++ ingles.py
@@ -0,0 +1,25 @@
+op1 = input("Como se di casa en inglés??")
+if(op1 == "house"):
+    print("Felicidades é correcto")
+
+
+op2 = input("Como se di mano en inglés??")
+if(op2 == "hand"):
+    print("Felicidades é correcto")
+
+
+op3 = input("Como se di pierna en inglés??")
+if(op3 == "leg"):
+    print("Felicidades é correcto")
+
+
+op4 = input("Como se di ordenador en inglés??")
+if(op4 == "computer"):
+    print("Felicidades é correcto")
+
+
+op5 = input("Como se di raton en inglés??")
+if(op5 == "mouse"):
+    print("Felicidades é correcto")
+
+
