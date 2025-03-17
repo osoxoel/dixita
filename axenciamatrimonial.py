bcce624a0882753cba327d3d951715c67195cfe4
osoxoel <rialxoel@gmail.com>
2025-03-17 16:46:09 +0100

    Add files via upload

---
+++ axenciamatrimonial.py
@@ -0,0 +1,22 @@
+print("Os posibles prometidos de esta noche son Ivan, c, pedro, pita y dani")
+print("Encontra o teu prometido")
+ojos = input("Como quieres los ojos tu prometido negros o marrons?")
+if(ojos == "marrons"):
+    print("vale, bien seguimos con los prometidos con los ojos marrones")
+if(ojos == "negros"):
+    print("tu prometido es c")
+
+pais = input("De que país queres que sexa o teu prometido españa, islandia o marruecos??")
+if(pais == "españa"):
+    print("vale, bien seguimos con los prometidos de españa")
+if(pais == "islandia"):
+    print("tu prometido es pedro")
+if(pais == "marruecos"):
+    print("tu prometido es pita")
+
+deporte = input("Que deporte queres que xogue o teu prometido baloncesto o futbol??")
+if(deporte == "futbol"):
+    print("o teu prometido é dani")
+if(deporte == "baloncesto"):
+    print("o teu prometido é ivan")
+print("grazas por participar o desexamos o mellor a ti e ao teu prometido")
