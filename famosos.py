6f5a56401c30e2bfbfc6719dd16dae256866bd54
osoxoel <rialxoel@gmail.com>
2025-03-18 13:50:52 +0100

    Add files via upload

---
+++ famosos.py
@@ -0,0 +1,28 @@
+print("A que adivino o teu famoso favorito")
+genero = input("De que xenero é o teu famoso??")
+if(genero == "chico"):
+    print("pedri ")
+if(genero == "chica"):
+    print(" ")
+
+pais = input("De que pais é o teu famoso??")
+if(pais == "España"):
+    print("  ")
+if(pais == "argentina"):
+    print("gabriela sabatini")
+if(pais == "portugal"):
+    print("Vicky Montanari")
+
+pelo = input("Que cor de pelo ten o teu famoso??")
+if(pelo == "negro"):
+    print(" ")
+if(pelo == "rubio"):
+    print(" vanesa romero")
+if(pelo == "castaño"):
+    print("sandra gago")
+
+deporte = input("Que deporte practica o teu famoso??")
+if(deporte == "badminton"):
+    print("a tua famosa e carolina marin ")
+if(deporte == "atletismo"):
+    print("a tua famosa e ana peleteiro ")
